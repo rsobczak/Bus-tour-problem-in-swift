299c4f2070f4b13f52ee39ca08dec90443387c1f
Radoslaw Sobczak <r.sobczak@onet.eu>
2020-09-06 20:27:14 +0200

    Initial Commit

---
+++ Bus Tour Problem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bus Tour Problem
+//
+//  Created by Radoslaw Sobczak on 06/09/2020.
+//  Copyright © 2020 Radoslaw Sobczak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
